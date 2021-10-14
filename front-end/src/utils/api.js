3811fc48512b9895d0f0c55816b2729c3ef877e4
Dylan Harville <dr.dyloh@gmail.com>
2021-10-14 02:33:02 -0600

    changed react app api url to heroku server; removed trailing  "/"

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -7,7 +7,7 @@
  import axios from "axios";
  
  const API_BASE_URL =
-   process.env.REACT_APP_API_BASE_URL || "https://restaurant-res-back.herokuapp.com/";
+   process.env.REACT_APP_API_BASE_URL || "https://restaurant-res-back.herokuapp.com";
  
  /**
   * Defines the default headers for these functions to work with `json-server`
