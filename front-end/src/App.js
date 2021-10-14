4e2ada09311730e7775ef1735368fd4a8a9fea45
Dylan Harville <dr.dyloh@gmail.com>
2021-10-14 15:14:59 -0600

    changed url for api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
 
-useEffect(() => {
-  fetch('https://restaurant-res-back.herokuapp.com')
-    .then((res) => res.json())
-    .then((nme) => setName([nme]))
-}, [])
-
 /**
  * Defines the root application component.
  * @returns {JSX.Element}
