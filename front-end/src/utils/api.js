43d30b55af11d84f1a2de60a95d725473595a769
Dylan Harville <dr.dyloh@gmail.com>
2021-10-14 19:21:23 -0600

    db url update

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -7,7 +7,9 @@
  import axios from "axios";
  
  const API_BASE_URL =
- process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+ process.env.NODE_ENV === "production"
+   ? process.env.REACT_APP_API_BASE_URL
+   : "http://localhost:5000";
  
  /**
   * Defines the default headers for these functions to work with `json-server`
